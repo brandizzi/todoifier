4e92fc056da9ab8d5bd4281605b63e1a261a9cec
Adam Brandizzi <adam.brandizzi@liferay.com>
2015-04-02 11:56:45 -0300

    #1 An assert function and a test runner created

---
+++ tests/testutil.js
@@ -0,0 +1,43 @@
+var DEFAULT_MESSAGE = 'Assertion failed';
+
+var assert = function assert(condition, message) {
+    if (!condition) {
+        if (typeof message == 'undefined') {
+            message = DEFAULT_MESSAGE;
+        }
+
+        throw new Error(message);
+    }
+};
+
+var TestRunner = function TestRunner(suite) {
+    this._suite = suite;
+    this._init();
+};
+
+TestRunner.prototype = {
+    _init: function() {
+        this.failCount = 0;
+        this.passCount = 0;
+        this.errors = {};
+    },
+
+    run: function() {
+        var suite = this._suite;
+        this._init();
+
+        for (var test in suite) {
+            if (test.indexOf('test') === 0) {
+                try {
+                    suite[test]();
+                    this.passCount++;
+                } catch (e) {
+                    this.errors[test] = e;
+                    this.failCount++;
+                }
+            }
+        }
+
+        return this;
+    }
+};

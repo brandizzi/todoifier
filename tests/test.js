3643702b967d6b0e37c3dd4b43704739f74198f4
Adam Brandizzi <adam.brandizzi@liferay.com>
2015-04-02 13:18:53 -0300

    #3 Adding taslk extractor

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 var tests = {
 
-    testCallRowExtractor: function() {
+    testRowExtractor: function() {
         var todoifier = new Todoifier({
             sourceElement: document.getElementById('content'),
             rowExtractor: function(src) {
@@ -9,6 +9,34 @@ var tests = {
         });
 
         assert(todoifier.tasks.length === 3);
+    },
+
+    testTaskExtractor: function() {
+        var todoifier = new Todoifier({
+            sourceElement: document.getElementById('content'),
+            rowExtractor: function(src) {
+                return src.getElementsByTagName('tr');
+            },
+            taskExtractor: function(rowNode) {
+                var tds = rowNode.getElementsByTagName('td');
+                return {
+                    description: tds.item(1).innerHTML,
+                    assignedTo: tds.item(2).innerHTML
+                };
+            }
+        });
+
+        var task = todoifier.tasks[0];
+        assert(task.description === 'Write test document');
+        assert(task.assignedTo === 'Alice');
+
+        task = todoifier.tasks[1];
+        assert(task.description === 'Write test case');
+        assert(task.assignedTo === 'Bob');
+
+        task = todoifier.tasks[2];
+        assert(task.description === 'Make test pass');
+        assert(task.assignedTo === 'Eve');
     }
 
 };
--- todoifier.js
+++ todoifier.js
@@ -1,4 +1,22 @@
 var Todoifier = function Todoifier(config) {
-    this.config = config;
-    this.tasks = this.config.rowExtractor(this.config.sourceElement);
+    this.config = {
+        rowExtractor: function() {
+            return [];
+        },
+        taskExtractor: function() {
+            return {};
+        },
+        sourceElement: document
+    };
+
+    for (var k in config) {
+        this.config[k] = config[k];
+    }
+    this.tasks = [];
+
+    var rows = this.config.rowExtractor(this.config.sourceElement);
+    for (var i = 0; i < rows.length; i++) {
+        var row = rows[i];
+        this.tasks.push(this.config.taskExtractor(row));
+    }
 };

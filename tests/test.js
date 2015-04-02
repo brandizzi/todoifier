0d8b32c01d0728798c97d7e2c2370eb08a53aff3
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2015-04-02 12:57:12 -0300

    #3 New test

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,16 @@
 var tests = {
+
+    testCallRowExtractor: function() {
+        var todoifier = new Todoifier({
+            sourceElement: document.getElementById('content'),
+            rowExtractor: function(src) {
+                return src.getElementsByTagName('tr');
+            }
+        });
+
+        assert(todoifier.tasks.length === 3);
+    }
+
 };
 
 window.onload = function() {
